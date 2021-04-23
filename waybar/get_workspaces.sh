c39160e3a52b7e2f970b0957631073fd6463585c
Soc Virnyl S. Estela <66054069+ReneganRonin@users.noreply.github.com>
2021-04-23 13:51:33 +0800

    Update waybar

--- waybar/get_workspaces.sh
+++ waybar/get_workspaces.sh
@@ -1,3 +1,9 @@
 workspace=$(swaymsg -t get_workspaces | jq -r '.[] | select(.focused)' | jq -r '.name')
+binding_status=$(swaymsg -t get_binding_state | jq -r '.name')
+if [[ $binding_status == "resize" ]]
+then
+    echo -e "視 Resize mode"
+else
+    echo -e "  Workspace $workspace"
+fi
 
-echo -e "  Workspace $workspace"
